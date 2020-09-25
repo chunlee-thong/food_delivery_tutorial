4db8cf7033e9f7dad8489259893008d3455c9ec2
Chunlee Thong <chunlee.thong@users.noreply.github.com>
2020-09-25 21:42:43 +0700

    Update food_card.dart

--- lib/widgets/food_card.dart
+++ lib/widgets/food_card.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class FoodCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card();
   }
 }
